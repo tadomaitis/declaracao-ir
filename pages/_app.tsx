3b40bba26a0b1704cb639c7d325e3425dcf81a58
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2022-02-25 19:24:22 -0300

    prepare app to use multiple layouts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
+import type { ReactElement, ReactNode } from 'react'
 import type { AppProps } from 'next/app'
+import type { NextPage } from 'next'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout): unknown {
+  const getLayout = Component.getLayout ?? ((page) => page)
+
+  return getLayout(<Component {...pageProps} />)
 }
 
 export default MyApp
