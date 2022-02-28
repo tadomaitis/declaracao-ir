fa59edff30fed293fca79cb666b6c459e8812181
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2022-02-28 15:49:01 -0300

    create SelectOption component

---
+++ components/SelectOption/SelectOption.tsx
@@ -0,0 +1,24 @@
+import styles from './SelectOption.module.css'
+import Image from 'next/image'
+
+type Props = {
+  label: string
+  selected: boolean
+  onClick: () => void
+}
+
+export function SelectOption({ label, selected, onClick }: Props) {
+  return (
+    <div
+      className={`${styles.selectOption} ${
+        selected ? styles.selectedOption : ''
+      }`}
+      onClick={onClick}
+    >
+      <div className={styles.selectIcon}>
+        {selected && <Image src="/svgs/tick.svg" width={18} height={18} />}
+      </div>
+      <p>{label}</p>
+    </div>
+  )
+}
