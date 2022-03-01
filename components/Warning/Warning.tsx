4b23149cbeff1fe641f067083cd94f8d4942b407
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2022-03-01 18:08:34 -0300

    style warning title

--- components/Warning/Warning.tsx
+++ components/Warning/Warning.tsx
@@ -12,7 +12,7 @@ export function Warning({ title, content }: Props): JSX.Element {
     <div className={styles.attention}>
       <div className={styles.iconContainer}>
         <Image src="/svgs/warning.svg" width={24} height={24} />
-        <h4>{title}</h4>
+        <h4 className={styles.warningTitle}>{title}</h4>
         <Image src="/svgs/warning.svg" width={24} height={24} />
       </div>
       <p>{content}</p>
